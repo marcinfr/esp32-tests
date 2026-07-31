5301cf96eebd982e8cf11d504415715b0dc98d96
Marcin Frymark <m.frymark@alekseon.com>
2026-07-31 20:03:07 +0200

    changes

---
+++ Led_On_Bass_Level/Led_On_Bass_Level.ino
@@ -0,0 +1,53 @@
+#include <SPI.h>
+#include <SD.h>
+#include "Audio.h"
+
+#define SD_CS 5
+
+#define I2S_DOUT 22
+#define I2S_BCLK 26
+#define I2S_LRC 25
+
+#define LED_PIN 13
+
+Audio audio;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  Serial.println("Start");
+
+  SPI.begin(18, 19, 23, SD_CS);
+
+  if (!SD.begin(SD_CS)) {
+    Serial.println("SD NIE wykryta");
+    while (1);
+  }
+
+  Serial.println("SD OK");
+
+  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+
+  audio.setVolume(15);
+
+  audio.connecttoFS(SD, "/song01.mp3");
+
+  Serial.println("Start MP3");
+}
+
+void loop() {
+
+  audio.loop();
+
+  // Czy wykryto uderzenie basu?
+  if (audio.getBassLevel()) {
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else {
+    digitalWrite(LED_PIN, LOW);
+  }
+}
--- Test_SD_Card/Test_SD_Card.ino
+++ Test_SD_Card/Test_SD_Card.ino
@@ -5,12 +5,6 @@
 #define CS_PIN 5
 #define LED_PIN 13
 
-#define I2S_DOUT 22
-#define I2S_BCLK 26
-#define I2S_LRC 25
-
-Audio audio;
-
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
@@ -43,13 +37,8 @@ void setup() {
     f.close();
     Serial.println("Zapis OK");
     digitalWrite(LED_PIN, HIGH);
-
-    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-    audio.setVolume(15);   // 0-21
-    audio.connecttoFS(SD, "/song01.mp3");
   }
 }
 
 void loop() {
-  audio.loop();
 }
